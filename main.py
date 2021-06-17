e62a855bbc8b7d78106fdaa7a3c157f476782f7a
Sajjad Momeni <sajjad5221@gmail.com>
2021-06-17 11:56:11 +0430

    database config and make user table

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
+import models
 
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
-    return {"hello":"world"}
+    return {"hello": "world"}
+
+
+@app.get("/articles")
+def read_articles(status: bool, limit: Optional[int] = None):
+    return {"message": f"{limit},{status} of data"}
+
+
+# @app.post("/user")
+# def register(request: models.User):
+#     return {"email": request.email}
